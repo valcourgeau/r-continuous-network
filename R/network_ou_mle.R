397d81e8a3b41aee82260c3bc4ee86ed09919c87
Valentin Courgeau <valentin.courgeau5@imperial.ac.uk>
2018-12-18 09:56:49 +0000

    Implemented Matrix MLE + cleaned mle file

--- R/network_ou_mle.R
+++ R/network_ou_mle.R
@@ -363,8 +363,8 @@ NOUmatrix <- function(nw_topo, times, data, thresholds){
   nw_data <- matrix(0, ncol=d, nrow=N)
   nw_data[1,] <- nw_data_bm[1,]*sqrt(delta_t)
   
-  nw_q <- 0.1*nw_topo
-  diag(nw_q) <- 1
+  nw_q <- 3*nw_topo
+  diag(nw_q) <- 10
     
   for(index in 2:N){
     nw_data[index,] <-  nw_data[index-1,] - (nw_q %*% nw_data[index-1,]) * (times[index]-times[index-1]) + nw_data_bm[index,]
@@ -375,62 +375,3 @@ NOUmatrix <- function(nw_topo, times, data, thresholds){
   NOUmatrix(nw_topo = nw_q, times = times, data = nw_data, thresholds = thresholds)
 }
 
-
-library("Matrix")
-#set.seed(42)
-n.subgrid <- 1000
-
-levy_noise <- rnorm(n = n.horizon*n.subgrid*d, 
-                    mean = 0, sd = sigma*sqrt(1/(24*n.subgrid)))
-levy_noise <- matrix(levy_noise, ncol = d)
-temp <- nw_data[N,]
-pred <- matrix(0, ncol=d, nrow=n.horizon)
-for(i in 1:n.horizon){
-  subnoise <- levy_noise[(1+(i-1)*n.subgrid):(i*n.subgrid),]
-  pred[i,] <- as.vector(t(expm(-res$Q*delta_t) %*% temp)) +
-    colSums(t(expm(-sqrt(delta_t)*res$Q) %*% t(subnoise)))
-  temp <- pred[i,]
-}
-
-library(viridis)
-par(xpd=FALSE)
-col.palette <- viridis(d)
-rollback <- 50
-plot((N-rollback):(N), nw_data[(N-rollback):N,1], 
-     type="l", ylim=c(-5,20), xlim=c(N-rollback,N+1.25*n.horizon),
-     col = col.palette[1], lwd=2,
-     ylab="Value", xlab="Sample number (delta_t)")
-lines((N+1):(N+n.horizon), pred[,1], 
-      col = col.palette[1], lwd=2, lty=2)
-lines((N+1):(N+n.horizon), nw_data_all[((N_all-n.horizon+1):N_all),1], 
-      col = col.palette[1], lwd=2, lty=1)
-abline(v=N)
-for(j in 2:d){
-  lines((N-rollback):(N), nw_data[(N-rollback):N,j], 
-       type="l", ylim=c(-1,20), xlim=c(N-rollback,N),
-       col = col.palette[j], lwd=2)
-  lines((N+1):(N+n.horizon), nw_data_all[((N_all-n.horizon+1):N_all),j], 
-        col = col.palette[j], lwd=2, lty=1)
-  lines((N+1):(N+n.horizon), pred[,j], 
-        col = col.palette[j], lwd=2, lty=2)
-}
-
-res$MLE_wide
-L <- res$Q
-library(igraph)
-net<-graph_from_adjacency_matrix(L, mode = c("directed"), weighted = TRUE, add.colnames=NULL) 
-#Compute number of vertecis
-n_v<-gorder(net);
-
-summary(net)
-
-#Adding edge and vertex attributes:
-s<-sample(5, size=d, replace=TRUE) 
-V(net)$dealertype<-s
-
-#Assign vertex colour corresponding to dealertype
-V(net)$color <- ifelse(V(net)$dealertype %in% c(1,2,3, 4), "red", "lightblue")
-
-#Assign vertex size corresponding to indegree
-V(net)$size<-degree(net, mode="total")*6 #because 1 is a small size for a node, we multiply it by 3 to make it bigger
-plot.igraph(net,layout=layout.circle, edge.width=E(net)$weight/20, edge.arrow.size=0.7, edge.label=L[L>0])
