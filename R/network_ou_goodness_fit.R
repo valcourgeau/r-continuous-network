0948dada2341827c491916ecd88603a1ce3f6b8c
Valentin Courgeau <valentin.courgeau5@imperial.ac.uk>
2018-12-18 17:38:10 +0000

    Add 5-d example to GoF tet

--- R/network_ou_goodness_fit.R
+++ R/network_ou_goodness_fit.R
@@ -1,3 +1,6 @@
+setwd("~/GitHub/r-continuous-network/R/")
+source("R/package_to_load.R")
+
 ### Function to assess goodness-of-fit of MLE estimations
 # based on Renyi entropy and statistics (Morales et al. (2000))
 
@@ -17,13 +20,18 @@ NOUGof <- function(theta, theta_true, data, times, quiet=F){
   rv_value <- as.vector(t(theta_mat %*% k_n)) %*%(theta-theta_true)
   
   eigen_vals <- eigen(k_n, only.values = T)$value
-  first <- imhof(q=rv_value, lambda = eigen_vals)
+  first <- suppressWarnings(imhof(q=rv_value, lambda = eigen_vals))
   abserr <- first$abserr
   first <- 1-first$Qq
-  last <- imhof(q=rv_value, lambda = eigen_vals)$Qq
+  last <- suppressWarnings(imhof(q=rv_value, lambda = eigen_vals)$Qq)
   
-  probability <- abs(last-first)
 
+  temp <- min(first, last)
+  last <- max(first, last)
+  first <- max(temp, 0.0) 
+
+  probability <- abs(last-first)
+  
   if(probability > 0.975){
     probability <- '***'
   }else{
@@ -32,19 +40,72 @@ NOUGof <- function(theta, theta_true, data, times, quiet=F){
     }else{
       if(probability > 0.90){
         probability <- '*'
+      }else{
+        probability <- 'x'
       }
     }
   }
-  test_values <- list("lower"=first,
-                     "upper"=last,
-                     "97.5%='***' / 95%='**' / 90%='*'"=probability,
-                     "Abs.error"=abserr)
+  test_values <- list()
+  test_values[["lower"]] <- first
+  test_values[["upper"]] <-last
+  test_values[["rating"]] <-as.character(probability)
+  test_values[["abs.error"]] <-abserr
+                     
   if(!quiet){
     cat("Goodness-of-test based on Renyi statistics\n")
+    print("97.5%='***' / 95%='**' / 90%='*'")
   }
+  
   return(test_values)
 }
 
 # https://cran.r-project.org/web/packages/CompQuadForm/CompQuadForm.pdf
-ans <- NOUGof(theta = mle_matrix_test, theta_true = nw_q, data=nw_data, times =times, quiet = T)
-ans
+{
+  source("network_generation.R")
+  d <- 5 #dims
+  N <- 24*1500 #numbers of points
+  M <- 1 # number of simulations
+  Y0 <- 1 #start point
+  delta_t <- 1/24
+  draw_n <- seq(from=8, to=N, by=1000)
+  mle_fit_example <- rep(0, length(draw_n)*M*2)
+  mle_fit_example <- array(data = mle_fit_example,
+                           dim=c(M,length(draw_n),2))
+  
+  sigma <- 0.1
+  set.seed(42)
+  nw_topo <- genRdmAssymetricGraphs(d = d, p.link = 0.25,
+                                    theta_1 = 1, theta_2 = 0)
+  nw_topo <- StdTopo(nw_topo)
+  
+  times <- seq(from = 0, by = delta_t, length.out = N)
+  thresholds <- rep(0, d)
+  for(i in 1:M){
+    nw_data_bm <- matrix(rnorm(n = d*N, mean = 0, sd = sigma*sqrt(delta_t)), ncol = d)
+    nw_data <- matrix(0, ncol=d, nrow=N)
+    nw_data[1,] <- nw_data_bm[1,]*sqrt(delta_t)
+    
+    nw_q <- 0.1*nw_topo
+    diag(nw_q) <- 1
+    
+    for(index in 2:N){
+      nw_data[index,] <-  nw_data[index-1,] - (nw_q %*% nw_data[index-1,]) * (times[index]-times[index-1]) + nw_data_bm[index,]
+    }
+    
+    #plot(nw_data[1:300,1], type="l")
+    
+    j <- 0
+    for(n_temp in draw_n){
+      res <- NOUfit(nw_topo = nw_topo, times = times[1:n_temp],
+                    data = nw_data[1:n_temp,], thresholds = thresholds)
+      mle_fit_example[i,j,] <- res$MLE_wide
+      
+      j <- j + 1
+    }
+  }
+  mle_true <- c(0.1, 1.0)
+  par(mfrow=(c(1,2)))
+  index_mle_param <- 1
+  
+  ans <- NOUGof(theta = mle_matrix_test, theta_true = nw_q, data=nw_data, times =times, quiet = T)
+}
