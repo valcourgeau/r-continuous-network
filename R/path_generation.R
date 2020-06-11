993f347f719dcdd5077dd24ff4421e67f5ed0e5f
Valentin Courgeau <valentin.courgeau@free.fr>
2020-06-11 16:11:59 +0100

    add generation of correlated bm

--- R/path_generation.R
+++ R/path_generation.R
@@ -18,3 +18,26 @@ ConstructPath <- function(nw_topo, noise, y_init, delta_time){
   
   return(d_y)  
 }
+
+
+CorrelatedBrownianNoise <- function(sigma_matrix, n_rows, delta_time){
+  n_nodes <- ncol(sigma_matrix)
+  assertthat::assert_that(n_nodes > 1)
+  sigma_matrix <- stats::cov2cor(sigma_matrix) # standard
+  uncorrelated_gaussian <- matrix(rnorm(n_rows*n_nodes, mean = 0, sd = sqrt(delta_time)), nrow=n_nodes)
+  print('ok')
+  sigma_matrix_copy <- sigma_matrix
+  diag(sigma_matrix_copy) <- 0.0
+  diag(sigma_matrix) <- sqrt(1-rowSums(sigma_matrix_copy^2))
+  sigma_matrix[upper.tri(sigma_matrix, diag = FALSE)] <- 0.0
+  corr_bm <- t(sigma_matrix %*% uncorrelated_gaussian)
+  print(corr_bm %>% dim)
+  integrated_bm <- apply(corr_bm, 2, cumsum)
+  return(integrated_bm)
+}
+
+set.seed(42)
+corr_bw <- CorrelatedBrownianNoise(sigma_matrix = matrix(c(1, 0, 0, 1), 2, 2), n_rows = 5e4, delta_time = 0.01)
+plot(corr_bw[,1], type='l')
+lines(corr_bw[,2])
+
